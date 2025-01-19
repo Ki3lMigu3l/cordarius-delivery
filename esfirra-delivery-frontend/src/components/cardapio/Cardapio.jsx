41e2af6bf18ef182ed2f5aecfde417e8feeffe5b
Ezequiel <ki3lmigu3l@gmail.com>
2025-01-19 15:40:00 -0300

    style: responsividade, ajuste do botão e margin do navbar

---
+++ esfirra-delivery-frontend/src/components/cardapio/Cardapio.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Cardapio.css'
+
+function Cardapio() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Cardapio
--- esfirra-delivery-frontend/src/pages/Cordarius.jsx
+++ esfirra-delivery-frontend/src/pages/Cordarius.jsx
@@ -2,21 +2,22 @@ import React from 'react'
 import '../styles/Cordarius.css'
 import Navbar from '../components/navbar/Navbar'
 import banner from '../assets/Banner-Cordarius-Desktop.png'
+import Cardapio from '../components/cardapio/Cardapio'
 
 const Cordarius = () => {
     
   return (
-    <div>
-      <Navbar/>
+    <><div>
+      <Navbar />
       <div className='cordarius-container'>
         <img className='cordarius-banner' src={banner} alt="" />
       </div>
       <div className='cordarius-btn-container'>
-            <button className='cordarius-btn'>Faça o seu pedido!
-            <i className="fa-solid fa-cart-shopping gap-btn"></i>
-            </button>
+        <button className='cordarius-btn'>Faça o seu pedido!
+          <i className="fa-solid fa-cart-shopping gap-btn"></i>
+        </button>
       </div>
-    </div>
+    </div><Cardapio /></>
   )
 }
 
